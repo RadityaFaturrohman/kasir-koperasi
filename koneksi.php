1b769c31e2dc83d76de54d17a160eb467b4fd517
rizky2016 <riri.rizky06@gmail.com>
2022-10-03 15:13:44 +0700

    datauser

---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php
+
+    $host = "localhost";         
+    $user = "root";
+    $password = "";
+    $database = "db_koperasi";
+
+    $connect = mysqli_connect($host, $user ,$password ,$database) or die ("gagal menghubungkan");
+
+?>
